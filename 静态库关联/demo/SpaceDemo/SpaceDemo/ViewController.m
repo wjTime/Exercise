5f835b8b8994e514a9adcd67942bfab4d9907865
高文杰 <yidong-ios16@yhd.com>
2016-10-06 15:17:19 +0800

    static lib

---
+++ 静态库关联/demo/SpaceDemo/SpaceDemo/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  SpaceDemo
+//
+//  Created by wenjie on 16/10/6.
+//  Copyright © 2016年 github. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "AFNetworking.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
