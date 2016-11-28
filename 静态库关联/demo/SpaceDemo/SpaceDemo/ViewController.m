759ae16153ddcd9d135895ded97cd14a1997eb23
高文杰 <yidong-ios16@yhd.com>
2016-11-28 17:57:46 +0800

    change
    change

--- 静态库关联/demo/SpaceDemo/SpaceDemo/ViewController.m
+++ 静态库关联/demo/SpaceDemo/SpaceDemo/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-#import "StaticLib/StaticLib.h"
+//#import "StaticLib/StaticLib.h"
 
 
 @interface ViewController ()
@@ -27,6 +27,61 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    /*
+    platform :ios, '8.0'
+#use_frameworks!
+    
+    workspace '../xxxxxDemo.xcworkspace'
+    
+    pod 'JSONModel'
+    pod 'CocoaLumberjack'
+    pod 'MagicalRecord'
+    pod 'AFNetworking'
+    pod 'SDWebImage'
+    pod 'SDWebImage/WebP'
+    pod 'KVOController'
+    pod 'PureLayout'
+    pod 'MBProgressHUD'
+    pod 'ReactiveCocoa', '~>2.5'
+    pod 'Toast'
+    
+    target 'OTSKit' do
+        project '../OTSKit/xxxxKit.xcodeproj'
+        end
+        
+        target 'OneStoreLight' do
+            project '../xxxxxDemo/xxxxxDemo.xcodeproj'
+            
+            
+            pod 'Fabric'
+            pod 'Crashlytics'
+            
+            pod 'Aspects'
+            pod 'MSWeakTimer'
+            pod 'RegexKitLite'
+            pod 'NSData+Base64'
+            pod 'ZipArchive'
+            
+            pod 'Masonry'
+            pod 'UIView+FDCollapsibleConstraints'
+            pod 'UITableView+FDTemplateLayoutCell'
+            pod 'TZImagePickerController'
+            
+            pod 'Nimbus/Core'
+            pod 'Nimbus/Interapp'
+            pod 'Nimbus/Models'
+            pod 'Nimbus/Collections'
+            
+#JSPatch
+#pod 'JSPatch'
+            
+#新浪微博
+#pod "WeiboSDK", :git => "https://github.com/sinaweibosdk/weibo_ios_sdk.git"
+            
+            end
+            */
+    
+    
 }
 
 
